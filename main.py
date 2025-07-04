fb1eefb20a4c56ec0b8f0007f45aac91e1e02f56
phazejeff <85849384+phazejeff@users.noreply.github.com>
2025-07-04 10:42:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -29,24 +29,24 @@ def index():
 def start(server: str):
     map = request.args.get("map")
     if map and map in MAPS:
-        subprocess.call(["echo", f"MAP={map}", "cs2-server", f"@{server}", "start"])
+        subprocess.call([f"MAP={map}", "cs2-server", f"@{server}", "start"])
         flash(f"Started server {server} with map {map}")
     elif server in SERVERS:
-        subprocess.call(["echo", "cs2-server", f"@{server}", "start"])
+        subprocess.call(["cs2-server", f"@{server}", "start"])
         flash(f"Started server {server}")
     return redirect(url_for("index"))
 
 @app.route("/stop/<server>")
 def stop(server: str):
     if server in SERVERS:
-        subprocess.call(["echo", "cs2-server", f"@{server}", "stop"])
+        subprocess.call(["cs2-server", f"@{server}", "stop"])
         flash(f"Stopped server {server}")
     return redirect(url_for("index"))
 
 @app.route("/restart/<server>")
 def restart(server: str):
     if server in SERVERS:
-        subprocess.call(["echo", "cs2-server", f"@{server}", "restart"])
+        subprocess.call(["cs2-server", f"@{server}", "restart"])
         flash(f"Restarted server {server}")
     return redirect(url_for("index"))
 
