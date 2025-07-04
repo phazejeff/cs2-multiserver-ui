26fade3e1a2fc0fd25bd62c3ad528cb2c6140dd0
phazejeff <85849384+phazejeff@users.noreply.github.com>
2025-07-04 12:44:45 -0700

    fixed map start

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, redirect, request, url_for, flash
 import subprocess
+import asyncio
 
 app = Flask(__name__)
 app.secret_key = 'suck my fat hairy sweaty balls, por favor'
 
-MAPS = [
-    "de_foroglio",
-    "de_lake",
-    "de_palais",
-    "de_eldorado",
-    "de_dogtown",
-    "de_neptune",
-    "de_splat"
-]
+MAPS = {
+    "de_foroglio" : "3132854332",
+    "de_lake" : "3219506727",
+    "de_palais" : "3257582863",
+    "de_eldorado" : "3408790618",
+    "de_dogtown": "3414036782",
+    "de_neptune" : "3430103877",
+    "de_splat" : "3439120481"
+}
 
 SERVERS = [
     "game1",
@@ -23,13 +24,14 @@ SERVERS = [
 
 @app.route("/")
 def index():
-    return render_template("index.html", maps=MAPS, servers=SERVERS)
+    return render_template("index.html", maps=MAPS.keys(), servers=SERVERS)
 
 @app.route("/start/<server>")
 def start(server: str):
     map = request.args.get("map")
     if map and map in MAPS:
-        subprocess.call([f"MAP={map}", "cs2-server", f"@{server}", "start"])
+        subprocess.call(["cs2-server", f"@{server}", "start"])
+        asyncio.run(changemap(server, map))
         flash(f"Started server {server} with map {map}")
     elif server in SERVERS:
         subprocess.call(["cs2-server", f"@{server}", "start"])
@@ -50,5 +52,9 @@ def restart(server: str):
         flash(f"Restarted server {server}")
     return redirect(url_for("index"))
 
+async def changemap(server:str, map: str):
+    await asyncio.sleep(2)
+    subprocess.call(["cs2-server", f"@{server}", "exec", "host_workshop_map", f"{MAPS[map]}"])
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
